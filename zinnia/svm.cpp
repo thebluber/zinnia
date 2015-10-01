6c9a224688f8610f4928e961c00db49d64c48225
Jiayi Zheng <thebluber@gmail.com>
2015-10-01 17:53:41 +0200

    Add comments to the source code

--- zinnia/svm.cpp
+++ zinnia/svm.cpp
@@ -25,6 +25,12 @@ bool svm_train(size_t l,
                const FeatureNode **x,
                double C,
                double *w) {
+  //l: y.size number of characters to train
+  //n: w.size
+  //*y: vector of +1.0 and -1.0
+  //**x: vector of features
+  //C: 1.0 <- regularization parameter 
+  //*w: empty vector of length n
   size_t active_size = l;
   double PGmax_old = kINF;
   double PGmin_old = -kINF;
@@ -32,9 +38,15 @@ bool svm_train(size_t l,
   std::vector<size_t> index(l);
   std::vector<double> alpha(l);
 
+  //length n is max_dim + 1, set in Trainer::add(character)
   std::fill(w, w + n, 0.0);
   std::fill(alpha.begin(), alpha.end(), 0.0);
 
+  //Newton-Raphson method is used below to determine alpha with the inequality constraint:
+  // 0 <= alpha <= C
+
+  //QD = x'x
+  //dotproduct of feature vector with itself which serves probably as a constant representing the 2. partial derivative of the Lagrangian Term?
   for (size_t i = 0; i < l; ++i) {
     index[i] = i;
     QD[i] = 0;
@@ -47,12 +59,20 @@ bool svm_train(size_t l,
   for (size_t iter = 0; iter < kMaxIteration; ++iter) {
     double PGmax_new = -kINF;
     double PGmin_new = kINF;
+    //random shuffle index from 0 to l
+    //rearrange the elements in the index
+    //what is the purpose of this?
     std::random_shuffle(index.begin(), index.begin() + active_size);
 
+    //for each character represented by i
+    //calculate the coefficient alpha according to the Newton-Raphson method:
+    //alpha_new = alpha_old - L(alpha)'/L(alpha)''
+    //and the corresponding w = sum(alpha_i y_i x_i)
     for (size_t s = 0; s < active_size; ++s) {
       const size_t i = index[s];
       double G = 0;
 
+      std::cout << "Index" << i << "\n";
       for (const FeatureNode *f = x[i]; f->index >= 0; ++f) {
         G += w[f->index] * f->value;
       }
@@ -80,19 +100,25 @@ bool svm_train(size_t l,
         }
       } else {
         PG = G;
-      }
+      } 
 
       PGmax_new = std::max(PGmax_new, PG);
       PGmin_new = std::min(PGmin_new, PG);
 
       if (std::abs(PG) > 1.0e-12) {
         const double alpha_old = alpha[i];
+        //the constraint 0 <= alpha <= C
+        //alpha_new = alpha_old - G/QD
         alpha[i] = std::min(std::max(alpha[i] - G/QD[i], 0.0), C);
+
+        //d = G/QD * y[i]
+        //why subtract alpha_old?
         const double d = (alpha[i] - alpha_old)* y[i];
         for (const FeatureNode *f = x[i]; f->index >= 0; ++f) {
           w[f->index] += d * f->value;
         }
       }
+      
     }
 
     if (iter % 4 == 0) {
